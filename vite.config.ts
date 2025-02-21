13548d88f5b08277999e4ccd35b97ea7c815b606
Sadikovic Rusmir <sesmanovic@gmail.com>
2025-02-21 15:17:24 +0100

    update vite config"

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+  server: {
+    host: "0.0.0.0", // Listen on all network interfaces
+    port: 5173, // Optional: Define a specific port
+  },
+
   plugins: [tailwindcss()],
 });
